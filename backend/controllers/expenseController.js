5bc60321f15bdae469a1727199f99f56d19bd496
Administrator <kandpalharayu@gmail.com>
2025-09-01 06:50:15 +0530

    expense_APIs_added

--- backend/controllers/expenseController.js
+++ backend/controllers/expenseController.js
@@ -1,71 +1,71 @@
 const xlsx = require("xlsx");
-const Income = require("../models/Income");
+const Expense = require("../models/Expense");
 
-// Add Income Source
-exports.addIncome = async (req, res) => {
+// Add Expense Source
+exports.addExpense = async (req, res) => {
     const userId = req.user.id;
 
     try {
-        const { icon, source, amount, date } = req.body;
-        if (!source || !amount || !date) {
+        const { icon, category, amount, date } = req.body;
+        if (!category || !amount || !date) {
             return res.status(400).json({ message: "Please fill all required fields" });
         }
 
-        const newIncome = new Income({
+        const newExpense = new Expense({
             userId,
             icon,
-            source,
+            category,
             amount,
             date: new Date(date)
         });
 
-        await newIncome.save();
-        res.status(201).json(newIncome);
+        await newExpense.save();
+        res.status(201).json(newExpense);
     } catch (error) {
         res.status(500).json({ message: "Server Error", error: error.message });
     }
-};
+}
 
-// Get All Income Source
-exports.getAllIncome = async (req, res) => {
+// Get All Expense Source
+exports.getAllExpense = async (req, res) => {
     const userId = req.user.id;
     try {
-        const incomes = await Income.find({ userId }).sort({ date: -1 });
-        res.status(200).json(incomes);
+        const expense = await Expense.find({ userId }).sort({ date: -1 });
+        res.status(200).json(expense);
     } catch (error) {
         res.status(500).json({ message: "Server Error", error: error.message });
     }
 };
 
-// Delete Income Source
-exports.deleteIncome = async (req, res) => {
+// Delete Expense Source
+exports.deleteExpense = async (req, res) => {
 
     try {
-        await Income.findOneAndDelete(req.params.id);
-        res.json({ message: "Income deleted successfully" });
+        await Expense.findOneAndDelete(req.params.id);
+        res.json({ message: "Expense deleted successfully" });
     } catch (error) {
         res.status(500).json({ message: "Server Error", error: error.message });
     }
 };
 
 // download Excel
-exports.downloadIncomeExcel = async (req, res) => {
+exports.downloadExpenseExcel = async (req, res) => {
     const userId = req.user.id;
     try {
-        const income = await Income.find({ userId }).sort({ date: -1 });
+        const expense = await Expense.find({ userId }).sort({ date: -1 });
 
         //prepare data for excel
-        const excelData = incomes.map((item)=>({
-            Source: item.source,
+        const excelData = expense.map((item)=>({
+            Category: item.category,
             Amount: item.amount,
             Date: item.date.toISOString().split('T')[0], //format date as YYYY-MM-DD
         }));
 
         const wb = xlsx.utils.book_new();
         const ws = xlsx.utils.json_to_sheet(excelData);
-        xlsx.utils.book_append_sheet(wb, ws, "income");
-        xlsx.writeFile(wb, "income_details.xlsx");
-        res.download("income_details.xlsx");
+        xlsx.utils.book_append_sheet(wb, ws, "expense");
+        xlsx.writeFile(wb, "expense_details.xlsx");
+        res.download("expense_details.xlsx");
     } catch (error) {
         res.status(500).json({ message: "Server Error", error: error.message });
     }
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const path = require("path");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const incomeRoutes = require("./routes/incomeRoutes");
+const expenseRoutes = require("./routes/expenseRoutes");
 
 
 const app = express();
@@ -24,6 +25,7 @@ connectDB();
 
 app.use("/api/v1/auth", require("./routes/authRoutes"));
 app.use("/api/v1/income", require("./routes/incomeRoutes"));
+app.use("/api/v1/expense", require("./routes/expenseRoutes"));
 
 //server uploads folder
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
